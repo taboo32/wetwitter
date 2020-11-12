c61a8eb22d6ef1dbd87191fd68e3a4259277413b
taboo32 <byeongkicho@gmail.com>
2020-11-12 17:55:58 +0900

    Set UP & Authentication

---
+++ src/routes/Profile.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { authService } from "fbase"
+import { useHistory } from "react-router-dom"
+
+export default () => {
+    const history = useHistory();
+    const onLogOutClick = () => {
+        authService.signOut();
+        history.push("/");
+    };
+    return (
+        <>
+            <button onClick={onLogOutClick}>
+                Log Out
+            </button>
+        </>
+    );
+};
