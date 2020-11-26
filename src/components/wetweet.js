6c889c2d9ecff392196383de6d99c016062baa4e
taboo32 <byeongkicho@gmail.com>
2020-11-26 23:57:49 +0900

    #4.2 Uploading a Pic & Thumbnail

--- src/components/wetweet.js
+++ src/components/wetweet.js
@@ -1,14 +1,55 @@
-import React from "react";
+import { dbService } from "fbase";
+import React, { useState } from "react";
 
-const Wetweet = ({ wetweetObj, isOwner }) => (
-    <div>
-        <h4>{wetweetObj.text}</h4>
-        {isOwner && (
-            <> 
-                <button>Delete Wetweet</button>
-                <button>Edit Wetweet</button>
+const Wetweet = ({ wetweetObj, isOwner }) => {
+    const [editing, setEditing] = useState(false);
+    const [newWetweet, setNewWetweet] = useState(wetweetObj.text);
+    const onDeleteClick = async () => {
+        const ok = window.confirm("Are you sure you want to delete this this?");
+        if (ok) {
+            await dbService.doc(`wetweets/${wetweetObj.id}`).delete();
+        }
+    };
+    const toggleEditing = () => setEditing((prev) => !prev);
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await dbService.doc(`wetweets/${wetweetObj.id}`).update({
+            text:newWetweet
+        });
+        setEditing(false);
+    };
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setNewWetweet(value);
+    }
+    return (
+        <div>
+            {editing ? (
+                <>
+                    {isOwner && (
+                    <>
+                    <form onSubmit={onSubmit}>
+                        <input type="text" placeholder="Edit your Wetweet" value={newWetweet} required onChange={onChange} />
+                        <input type="submit" value="Update Wetweet" />
+                    </form>
+                    <button onClick={toggleEditing}> Cancel </button>
+                </>
+                )}
             </>
-        )}
-    </div>
-);
+            ) : (
+                <>
+                    <h4>{wetweetObj.text}</h4>
+                    {isOwner && (
+                        <> 
+                            <button onClick={onDeleteClick} >Delete Wetweet</button>
+                            <button onClick={toggleEditing} >Edit Wetweet</button>
+                        </>
+                    )}
+                </>
+            )}
+        </div>
+    );
+}
 export default Wetweet;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,6 +5,7 @@ import Wetweet from "components/wetweet";
 const Home = ({ userObj }) => {
     const [wetweet, setWetweet] = useState("");
     const [wetweets, setWetweets] = useState([]);
+    const [attachment, setAttachment] =useState();
 
     useEffect(() => {
         dbService.collection("wetweets").onSnapshot((snapshot) => {
@@ -29,12 +30,32 @@ const Home = ({ userObj }) => {
         } = event;
         setWetweet(value);
     };
-    console.log(wetweets);
+    const onFileChange = (event) => {
+        const {
+            target: { files },
+        } = event;
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            const {currentTarget: { result },
+            } = finishedEvent;
+            setAttachment(result)
+        }
+        reader.readAsDataURL( theFile );
+    }
+    const onClearAttachment = () => setAttachment(null)
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input value={wetweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+                <input type="file" accept="image/*" onChange={onFileChange} />
                 <input type="submit" value="Wetweet"/>
+                {attachment && (
+                    <div>
+                        <img src={attachment} width="50px" height="50px" />
+                         <button onClick={onClearAttachment}> Clear </button>
+                    </div>
+                )}
             </form>
             <div>
                 {wetweets.map((wetweet) => (
